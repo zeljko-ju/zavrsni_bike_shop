50ad69ff3a25ae86b0a831ee635fca0ac1333ae4
Zeljko-ju <zeljac00@icloud.com>
2022-05-31 21:50:02 +0200

    update, dodani artikli
    dodane vrijednosti artikala i odrađena select naredba na više baza

--- zavrsni_rad_bike_shop.sql
+++ zavrsni_rad_bike_shop.sql
@@ -1,4 +1,4 @@
-#C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\php\Documents\GitHub\zavrsni_bike_shop\zavrsni_rad_bike_shop.sql
+#C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\php\Documents\GitHub\zavrsni_bike_shop_\zavrsni_rad_bike_shop.sql
 
 drop database if exists bike_shop;
 create database bike_shop;
@@ -36,8 +36,10 @@ create table racun (
 create table artikl (
     sifra int not null primary key auto_increment,
     brand varchar(50),
+    kategorija varchar(50),
+    model varchar(50),
     cijena decimal(18,2),
-    dostupnost boolean
+    dostupnost boolean not null
 
 
 );   
@@ -49,13 +51,21 @@ alter table stavka add foreign key (artikl) references artikl(sifra);
 alter table stavka add foreign key (racun) references racun(sifra);
 
 
-insert into artikl (sifra, brand, cijena, dostupnost)
-	values (null, 'fuji', 7999.99, true);
+insert into artikl (sifra, brand, kategorija, model, cijena, dostupnost)
+	values (null, 'cube', 'MTB', 'Aim RACE', 4999.99, true),
+        (null, 'cube', 'MTB', 'Access WS', 4099.99, true),
+        (null, 'cube', 'MTB', 'Accsess WS PRO', 5999.99, true),
+        (null, 'cube', 'MTB', 'Aim', 3999.99, true),
+        (null, 'cube', 'MTB', 'Aim SL', 6999.99, true),
+        (null, 'cube', 'MTB', 'Nature', 5999.99, true),
+        (null, 'cube', 'cestovni', 'Nuroad', 7999.99, true);
 insert into stavka (sifra, kolicina, artikl, broj_racuna)
 	values (null, 1, 1, '999lsjjs7788');
 
 insert into djelatnik (sifra, ime, prezime)
-	values (null, 'Tvrtko', 'Biciklović');
+	values (null, 'Tvrtko', 'Biciklović'), 
+        (null, 'Marko', 'Perišić'),
+        (null, 'Ivan', 'Jakobović');
 insert into kupac (sifra, ime, prezime)
 	values (null, 'Marko', 'Ivšić');
  insert into racun (sifra, djelatnik, kupac)
@@ -64,3 +74,13 @@ insert into kupac (sifra, ime, prezime)
  insert into stavka (sifra, kolicina, artikl, broj_racuna, racun)
  	values(null, 3, 1, 'klsakdjsdkasd', 1);
  	
+select * from racun;
+#odradio select naredbu za spajanje podataka
+
+select a.sifra, a.kolicina, d.brand, d.cijena, a.broj_racuna, c.ime, c.prezime 
+from stavka a left join racun b
+on a.racun =b.sifra 
+inner join djelatnik c
+on b.djelatnik = c.sifra
+inner join artikl d 
+on a.artikl =d.sifra; 
